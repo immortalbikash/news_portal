26303e325c3aa0e1d390ddc055989fc701eb0c1c
Bikash <bikashkatwalcr7@gmail.com>
2026-02-02 18:07:42 -0800

    adding bootstrap in project

---
+++ src/pages/Mainpage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Mainpage = () => {
+    return (
+        <div>this is main page</div>
+    )
+}
+
+export default Mainpage
---
+++ src/routes/Router.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Router = () => {
+    return (
+        <>
+            Router page
+        </>
+    )
+}
+
+export default Router
